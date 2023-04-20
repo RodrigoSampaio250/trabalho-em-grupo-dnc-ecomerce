043ba691be399d5912ea885fa5e2e66c14bff600
RodrigoSampaio250 <rodrigosampaio828@gmail.com>
2023-04-19 23:44:36 -0300

    Add Home

---
+++ name-of-your-project/src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.scss';
+import Nav from '../../components/Nav/Nav';
+import ProdutoCard from '../../components/ProdutoCard/ProdutoCard'
+import Footer from '../../components/Footer/Footer'
+
+export const Home = ({dados}) => {
+  return (
+    <div className='home'>
+      <Nav />
+      <img className='home__propaganda' src="propaganda.svg" alt="" />
+      <div className='home__conteiner'>
+        {
+          dados.map(produto => (<ProdutoCard key={produto.id} dados={produto}/>))
+        }
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
